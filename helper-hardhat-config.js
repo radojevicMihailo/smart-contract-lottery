9e665612b3d8ff8bdee0b134f2bb9fb7bdbf1d0e
radojevicMihailo <radojevic.mihailo.00@gmail.com>
2022-11-16 12:12:02 +0100

    added contract address and abi

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -30,8 +30,8 @@ const networkConfig = {
 
 const developmentChains = ["hardhat", "localhost"]
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6
-const frontEndContractsFile = "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
-const frontEndAbiFile = "../nextjs-smartcontract-lottery-fcc/constants/abi.json"
+const frontEndContractsFile = "../nextjs-smart-contract-lottery/constants/contractAddresses.json"
+const frontEndAbiFile = "../nextjs-smartc-contract-lottery/constants/abi.json"
 
 module.exports = {
     networkConfig,
